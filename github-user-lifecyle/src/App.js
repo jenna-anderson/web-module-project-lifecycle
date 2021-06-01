17fe95dea5c4f57dc732f530983bdee6d48d11a2
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-06-01 16:51:01 -0600

    Added styling with react bootstrap

--- github-user-lifecyle/src/App.js
+++ github-user-lifecyle/src/App.js
@@ -5,8 +5,7 @@ import axios from 'axios'
 
 class App extends React.Component {
   state = {
-    userData: {},
-    followers: [],
+    userData: {}
   }
 
   componentDidMount() {
@@ -19,35 +18,13 @@ class App extends React.Component {
     .catch(err => {
       console.log(err)
     })
-
-    axios.get('https://api.github.com/users/jenna-anderson/followers')
-    .then(res => {
-      console.log(res.data)
-      this.setState({
-        followers: res.data
-      })
-    })
-    .catch(err => {
-      console.log(err)
-    })
   }
 
   render() {
-    console.log(this.state.userData)
     return (
       <div className="App">
         <h1>Github User</h1>
         <UserCard userData={this.state.userData}/>
-        {
-        this.state.followers.map(follower => {
-          return (
-            <div>
-            <h4>Followers:</h4>
-            <li><a href={follower.html_url}>{follower.login}</a></li>
-          </div>
-          )
-        })
-        }
       </div>
     );
   }
--- github-user-lifecyle/src/components/UserCard.js
+++ github-user-lifecyle/src/components/UserCard.js
@@ -1,16 +1,58 @@
 import React from 'react'
-import User from './User'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap'
+import axios from 'axios'
+import '../App.css'
 
 class UserCard extends React.Component {
+    state={
+        followers: [],
+        isActive: false
+    }
+
+    handleClick = (e) => {
+        this.setState({
+            isActive: !this.state.isActive
+        })
+    }
+
+    componentDidMount() {
+        axios.get('https://api.github.com/users/jenna-anderson/followers')
+        .then(res => {
+          console.log(res.data)
+          this.setState({
+            followers: res.data
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
     render() {
         return(
-            <div>
-                <h2>{this.props.userData.name}</h2>
-                <img src={this.props.userData.avatar_url} alt='user profile photo'/>
-                <p>followers: {this.props.userData.followers}</p>
-                <p>following: {this.props.userData.following}</p>
-                <p>username: {this.props.userData.login}</p>
-            </div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.userData.avatar_url} alt={this.props.userData.name} />
+                <Card.Body>
+                    <Card.Title>{this.props.userData.name}</Card.Title>
+                   
+                    <Card.Text>followers: {this.props.userData.followers}</Card.Text>
+                    <Card.Text>following: {this.props.userData.following}</Card.Text>
+                    <Card.Text><a href={this.props.userData.html_url}>{this.props.userData.login}</a></Card.Text>
+                    
+                <Button variant="primary" onClick={this.handleClick}>Followers</Button>
+                {
+                    this.state.followers.map(follower => {
+                        return (
+                        <Card.Body className={this.state.isActive ? null : 'hide'} key={follower.id}>
+                                <Card.Title>Followers:</Card.Title>
+                                <Card.Text><li><a href={follower.html_url}>{follower.login}</a></li></Card.Text>
+                        </Card.Body>
+                        )
+                    })
+                    }
+                </Card.Body>
+            </Card>
         )
     }
 }
